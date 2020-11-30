4e6844fcf119867bce092588382d29cc2b7cdf50
jossdz <carloscorrea.dev@gmail.com>
2020-11-30 10:55:03 -0600

    isAuth for create/detail jobs

--- server/routes/index.js
+++ server/routes/index.js
@@ -12,8 +12,8 @@ const { isAuth, catchErrs } = require('../middlewares')
 // ------------------JOB ROUTES------------------------
 
 router.get('/jobs', isAuth, catchErrs(getUserJobs));
-router.get('/jobs/:jobId', catchErrs(getJobDetails))
-router.post('/jobs', catchErrs(createJob))
+router.get('/jobs/:jobId', isAuth, catchErrs(getJobDetails))
+router.post('/jobs', isAuth, catchErrs(createJob))
 router.put('/jobs/:jobId', catchErrs(updateJob))
 router.delete('/jobs/:jobId', catchErrs(deleteJob))
 
